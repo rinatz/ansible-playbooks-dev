c5844158650f9eb2276cbc4c52f5f8dee224fcc4
井田 献一朗 <ida.kenichiro@gmail.com>
2022-10-15 18:14:37 +0900

    Refactor

--- ansible-playbook.sh
+++ ansible-playbook.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 set -eu
 
+THIS_SCRIPT_DIR=$(cd "$(dirname "$0")"; pwd)
+readonly THIS_SCRIPT_DIR
+
 function main() {
     local tags="${1:-all}"
 
-    ansible-playbook -i localhost, -c local site.yml --tags="${tags}"
+    ansible-playbook -i localhost, -c local "${THIS_SCRIPT_DIR}"/site.yml --tags="${tags}"
 }
 
 main "$@"
